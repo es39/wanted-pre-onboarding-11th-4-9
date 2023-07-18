f5d3cf466f67bf574d044a51e530ad39c3910889
es39 <esj10025@gmail.com>
2023-07-18 15:57:59 +0900

    :lipstick: Design : 메인 화면 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">hello world</div>;
-}
+import SearchBar from './components/SearchBar';
+import { Container, Content } from './styles/Style';
+
+const App = () => {
+  return (
+    <Container>
+      <Content>
+        <h2>
+          국내 모든 임상시험 검색하고 <br />
+          온라인으로 참여하기
+        </h2>
+        <SearchBar />
+      </Content>
+    </Container>
+  );
+};
 
 export default App;
