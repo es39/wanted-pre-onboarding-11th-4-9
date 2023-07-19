1acacfa8fdaae9edc21c5aa5b06852fb5480e662
es39 <esj10025@gmail.com>
2023-07-19 20:49:20 +0900

    :recycle: Refactor : 코드 정리

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -17,6 +17,13 @@ const SearchBar = () => {
   const [isSearching, setIsSearching] = useState(false);
   const [recommendSearch, setRecommendSearch] = useState<SickDataProps>();
 
+  useEffect(() => {
+    api.get('/sick').then(res => {
+      setSearchData(res.data);
+      console.info('calling api');
+    });
+  }, []);
+
   const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserInput(e.target.value);
     if (searchData) {
@@ -30,28 +37,12 @@ const SearchBar = () => {
     }
   };
 
-  // useEffect(() => {
-  //   const handleSearchKeyUpDown = (e: KeyboardEvent) => {
-  //     if (e.key === 'ArrowUp') {
-  //     }
-  //     if (e.key === 'ArrowDown') {
-  //     }
-  //   };
-  // }, []);
-
-  useEffect(() => {
-    api.get('/sick').then(res => {
-      setSearchData(res.data);
-      console.info('calling api');
-    });
-  }, []);
-
   const handleSearchDropdown = () => {
     setIsSearching(true);
   };
 
   return (
-    <FormContainer>
+    <FormContainer onSubmit={e => e.preventDefault}>
       <SearchContainer>
         <InputContainer>
           <AiOutlineSearch className="input-icon" />
@@ -78,10 +69,3 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
-/* TODO:
-1. 검색 기능 구현 -> 버튼 클릭 시, get 요청 보내고, params 내용과 일치하는 내용들 출력..?
-1-1. 검색어 없을 시, 검색어 없음 표현
-2. 드롭다운 구현
-3. 키보드 만으로 할 수 있도록 구현
-4. api 호출 시, console.info("calling api")
-*/
