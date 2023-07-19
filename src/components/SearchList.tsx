25ebc9ad2afb6cceb0cfc4a2521ac32938a55e24
es39 <esj10025@gmail.com>
2023-07-19 15:23:50 +0900

    :sparkles: Feat : 검색했을 시, 목록 띄워주기

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,5 +1,15 @@
-const SearchList = () => {
-  return <div>검색목록 보여줄 곳</div>;
+const SearchList = (searchData: any, isSearching: boolean) => {
+  return (
+    <>
+      {!isSearching ? (
+        searchData.length !== 0 ? (
+          searchData.map((el: any, idx: number) => <li key={idx}>{el[1]}</li>)
+        ) : (
+          <span>검색어 없음</span>
+        )
+      ) : null}
+    </>
+  );
 };
 
 export default SearchList;
